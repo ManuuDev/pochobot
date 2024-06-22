137a5782a3364f1b5faa9bb683ba3ffac643b7a1
manu <manuu.97@hotmail.com>
2024-06-21 21:52:52 -0300

    fix dollar tasks time

--- app/cogs/cog_tasks.py
+++ app/cogs/cog_tasks.py
@@ -1,5 +1,4 @@
 import datetime
-import pytz
 from discord.ext import commands, tasks
 from app.core.database import update_steam_database
 from app.core.finance_manager import get_dollar_quote
@@ -9,14 +8,15 @@ from app.system.utils import send_response, send_response_with_quote_format
 
 class TasksCog(commands.Cog):
 
+    channel_id_list_for_dollar_quotes : list = ['995207211297013791']
+
     def __init__(self, bot):
         self.bot = bot
         self.check_empty_voice_channel_task.start()
         self.update_steam_database_task.start()
         self.dollar_quote_task_opening.start()
         self.dollar_quote_task_close.start()
-
-
+    
     @tasks.loop(minutes=1.0)
     async def check_empty_voice_channel_task(self):
         if len(self.bot.voice_clients) > 0 and len(self.bot.voice_clients[0].channel.members) == 1:
@@ -29,21 +29,29 @@ class TasksCog(commands.Cog):
         update_steam_database()
         log("Base de datos de steam actualizada")
 
-    @tasks.loop(time=datetime.time(hour=11,tzinfo=pytz.timezone('America/Argentina/Buenos_Aires')))
+    @tasks.loop(time=datetime.time(hour=14))
     async def dollar_quote_task_opening(self):
-        message = await get_dollar_quote()
-        ctx = await self.bot.fetch_channel('995207211297013791')
-        if message:
-            await send_response(ctx, 'Cotizaciones de apertura')
-            await send_response_with_quote_format(ctx, message)
+        global channel_id_list_for_dollar_quotes
 
-    @tasks.loop(time=datetime.time(hour=18,tzinfo=pytz.timezone('America/Argentina/Buenos_Aires')))
-    async def dollar_quote_task_close(self):
         message = await get_dollar_quote()
-        ctx = await self.bot.fetch_channel('995207211297013791')
-        if message:
-            await send_response(ctx, 'Cotizaciones de cierre')
-            await send_response_with_quote_format(ctx, message)
+        
+        for channel_id in channel_id_list_for_dollar_quotes:
+            ctx = await self.bot.fetch_channel(channel_id)
+            if message:
+                await send_response(ctx, 'Cotizaciones de apertura')
+                await send_response_with_quote_format(ctx, message)
+
+    @tasks.loop(time=datetime.time(hour=21))
+    async def dollar_quote_task_close(self):
+        global channel_id_list_for_dollar_quotes
+
+        message = await get_dollar_quote()
+
+        for channel_id in channel_id_list_for_dollar_quotes:
+            ctx = await self.bot.fetch_channel(channel_id)
+            if message:
+                await send_response(ctx, 'Cotizaciones de cierre')
+                await send_response_with_quote_format(ctx, message)
 
 
 def setup(bot):
